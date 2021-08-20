ebe036e5172bf67a57658d5f2cf53cdaad3c1f41
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-19 23:20:18 -0400

    Added category get route to get all categories in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', (req, { status }) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+    status(200).json(categoryData);
+  } catch (err) {
+    status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
